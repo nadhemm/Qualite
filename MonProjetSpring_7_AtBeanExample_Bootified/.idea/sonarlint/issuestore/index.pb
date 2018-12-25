024868f841cf9ad56fe62143f5732f052d879fe0
Nadhem <nadhem.m.2016@ieee.org>
2018-12-26 00:26:51 +0100

    added some employees

---
+++ MonProjetSpring_7_AtBeanExample_Bootified/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Y
+)src/main/java/tn/insat/model/Employe.java,b/3/b3d421a5826004b6a1ed50925e59916ff9726052
+]
+-src/main/java/tn/insat/MyBootApplication.java,5/c/5cad7f8743a7656e5b1a1a462cbbfe991b7282c1
--- MonProjetSpring_7_AtBeanExample_Bootified/src/main/java/tn/insat/MyBootApplication.java
+++ MonProjetSpring_7_AtBeanExample_Bootified/src/main/java/tn/insat/MyBootApplication.java
@@ -12,29 +12,33 @@ import java.util.List;
 @SpringBootApplication
 public class MyBootApplication {
 
-	//@Autowired
-	//Employe e;
-	@Bean     // This is a Producer
-	public Employe employe1() {
-		return new Employe("Ahmed", 2000);
-	}
+    //@Autowired
+    //Employe e;
+    @Bean     // This is a Producer
+    public Employe employe1() {
+        return new Employe("Ahmed", 2000);
+    }
 
-	@Bean
-	public Employe employe2() {
-		return new Employe("Fatma", 2000);
-	}
+    @Bean
+    public Employe employe2() {
+        return new Employe("Fatma", 2000);
+    }
 
-	@Bean
-	public List<Employe> staff() {
-		return Arrays.asList(employe1(), employe2());
-	}
+    @Bean
+    public Employe employe3() { return new Employe("Sami", 1500); }
 
-	public static void main(String[] args) {
-	   SpringApplication.run(MyBootApplication.class, args);
-	}
 
-	@Bean 
-	public CommandLineRunner run() {
-		return args -> 	System.out.println(staff()) ;
-	}
+    @Bean
+    public List<Employe> staff() {
+        return Arrays.asList(employe1(), employe2());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> System.out.println(staff());
+    }
 }
